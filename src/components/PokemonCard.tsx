59490940de335766b03c53f327a30301a6f08a3b
MonaEis <mona.eismann@gmx.de>
2025-04-10 17:10:52 +0200

    fetch

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,32 +1,36 @@
-import { useEffect, useState } from "react"
-import { PokemonDetails } from "../types/pokemonTypes"
-import { fetchPokemonDetails } from "../api/pokemonApi"
-
+import { useEffect, useState } from "react";
+import { PokemonDetails } from "../types/pokemonTypes";
+import { fetchPokemonDetails } from "../api/pokemonApi";
+import PokemonDetails from "./PokemonDetails";
 
 type PokemonCardProps = {
-    name: string,
-    url: string
-}
+    name: string;
+    url: string;
+};
+
+function PokemonCard({ name, url }: PokemonCardProps) {
+    const [pokemonDetails, setPokemonDetails] = useState<PokemonDetails | null>(
+        null
+    );
+
+    useEffect(() => {
+        fetchPokemonDetails(name).then((value) => {
+            setPokemonDetails(value);
+        });
+    }, [name]);
+
+    // console.log("Details: ", pokemonDetails)
+
+   
 
 
-
-
-function PokemonCard({name, url}: PokemonCardProps) {
-
-    const [pokemonDetails, setPokemonDetails] = useState<PokemonDetails|null>(null)
-
-    useEffect(()=>{
-        fetchPokemonDetails(name).then((value)=>{
-            setPokemonDetails(value)
-        })
-    }, [])
-    
-    console.log("Details: ", pokemonDetails)
     return (
         <div>
             <img src={pokemonDetails?.sprites.front_default} alt="" />
-            <h1>{name}</h1>
-            <p>{url}</p>
+            <div>
+            <p>#{String(pokemonDetails?.id).padStart(3, '0')}</p>
+                <p>{name}</p>
+            </div>
         </div>
     );
 }
