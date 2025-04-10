577ba2b064d10e2003273db072c5d4b94f9f74d5
MonaEis <mona.eismann@gmx.de>
2025-04-10 15:52:56 +0200

    homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+import { fetchPokemonList } from "../api/pokemonApi";
+import { PokemonListResponse } from "../types/pokemonTypes";
+
 function HomePage() {
+
+    const [pokemonList, setPokemonList] = useState<PokemonListResponse | null>(null) 
+
+    useEffect(()=>{
+        fetchPokemonList().then((value)=>{
+            setPokemonList(value)
+        })
+    },[])
+console.log(pokemonList?.results)
+
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+    {pokemonList.map((singlePokemon)=>{
+        <h1>{singlePokemon.name}</h1>
+    })
+    }
   );
 }
 
