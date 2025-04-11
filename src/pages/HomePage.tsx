bb3cac112f8a18aa694f1f77f3cb8fa4bd04195d
MonaEis <mona.eismann@gmx.de>
2025-04-11 11:58:28 +0200

    searchbar funciton

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,36 +1,47 @@
-import { useEffect, useState } from 'react';
-import { fetchPokemonList } from '../api/pokemonApi';
-import { PokemonListResponse } from '../types/pokemonTypes';
-import PokemonCard from '../components/PokemonCard';
-import './HomePage.css';
-import { Link } from 'react-router-dom';
-import Header from '../components/Header';
+import { useEffect, useState } from "react";
+import { fetchPokemonList } from "../api/pokemonApi";
+import { PokemonListResponse } from "../types/pokemonTypes";
+import PokemonCard from "../components/PokemonCard";
+import "./HomePage.css";
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
 
 function HomePage() {
-  const [pokemonList, setPokemonList] = useState<PokemonListResponse | null>(
-    null
-  );
+    const [pokemonList, setPokemonList] = useState<PokemonListResponse | null>(
+        null
+    );
+    const [searchInput, setSearchInput] = useState("");
 
-  useEffect(() => {
-    fetchPokemonList().then((value) => {
-      setPokemonList(value);
-    });
-  }, []);
+    useEffect(() => {
+        fetchPokemonList().then((value) => {
+            setPokemonList(value);
+        });
+    }, []);
 
-  return (
-    <>
-      <div>
-        <Header input={pokemonList} />
-      </div>
-      <div className="pokemon-list">
-        {pokemonList?.results.map((singlePokemon) => (
-          <Link key={singlePokemon.name} to={`/pokemon/${singlePokemon.name}`}>
-            <PokemonCard name={singlePokemon.name} url={singlePokemon.url} />
-          </Link>
-        ))}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <Header input={searchInput} setInput={setSearchInput} />
+            </div>
+            <div className="pokemon-list">
+                {pokemonList?.results
+                    .filter((pokemon) =>
+                        pokemon.name.includes(searchInput.toLowerCase())
+                    )
+                    .map((singlePokemon) => (
+                        <Link
+                            key={singlePokemon.name}
+                            to={`/pokemon/${singlePokemon.name}`}
+                        >
+                            <PokemonCard
+                                name={singlePokemon.name}
+                                url={singlePokemon.url}
+                            />
+                        </Link>
+                    ))}
+            </div>
+        </>
+    );
 }
 
 export default HomePage;
